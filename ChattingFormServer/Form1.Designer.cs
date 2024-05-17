cfdb45bba3a855b649e9d9fbb70dea3882d31a1a
Awseok <37686517+Awseok@users.noreply.github.com>
2024-05-17 15:02:27 +0900

    add file

---
+++ ChattingFormServer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace udpSocketTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_START = new System.Windows.Forms.Button();
+            this.RTB_HISTORY = new System.Windows.Forms.RichTextBox();
+            this.TXT_CHAT = new System.Windows.Forms.TextBox();
+            this.LST_LOG = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BTN_START
+            // 
+            this.BTN_START.Location = new System.Drawing.Point(12, 22);
+            this.BTN_START.Name = "BTN_START";
+            this.BTN_START.Size = new System.Drawing.Size(74, 24);
+            this.BTN_START.TabIndex = 0;
+            this.BTN_START.Text = "시작";
+            this.BTN_START.UseVisualStyleBackColor = true;
+            this.BTN_START.Click += new System.EventHandler(this.BTN_START_Click);
+            // 
+            // RTB_HISTORY
+            // 
+            this.RTB_HISTORY.Location = new System.Drawing.Point(12, 52);
+            this.RTB_HISTORY.Name = "RTB_HISTORY";
+            this.RTB_HISTORY.Size = new System.Drawing.Size(635, 210);
+            this.RTB_HISTORY.TabIndex = 1;
+            this.RTB_HISTORY.Text = "";
+            // 
+            // TXT_CHAT
+            // 
+            this.TXT_CHAT.Location = new System.Drawing.Point(12, 268);
+            this.TXT_CHAT.Name = "TXT_CHAT";
+            this.TXT_CHAT.Size = new System.Drawing.Size(635, 21);
+            this.TXT_CHAT.TabIndex = 2;
+            this.TXT_CHAT.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TXT_CHAT_KeyDown);
+            // 
+            // LST_LOG
+            // 
+            this.LST_LOG.FormattingEnabled = true;
+            this.LST_LOG.ItemHeight = 12;
+            this.LST_LOG.Location = new System.Drawing.Point(13, 297);
+            this.LST_LOG.Name = "LST_LOG";
+            this.LST_LOG.Size = new System.Drawing.Size(633, 88);
+            this.LST_LOG.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 412);
+            this.Controls.Add(this.LST_LOG);
+            this.Controls.Add(this.TXT_CHAT);
+            this.Controls.Add(this.RTB_HISTORY);
+            this.Controls.Add(this.BTN_START);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_START;
+        private System.Windows.Forms.RichTextBox RTB_HISTORY;
+        private System.Windows.Forms.TextBox TXT_CHAT;
+        private System.Windows.Forms.ListBox LST_LOG;
+    }
+}
+
---
+++ ChattingFormServer/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udpSocketTest
+{
+    public partial class Form1 : Form
+    {
+        private bool m_DEBUG_CHECK = true;
+        private string m_ip;
+        private int m_port;
+        private Thread m_listenThread;
+        private Thread m_receiveThread;
+        private Socket m_clientSocket;
+
+        private void setIPort()
+        {
+            if (m_DEBUG_CHECK)
+            {
+                m_ip = "127.0.0.1";
+                m_port = 25101;
+            }
+            //m_ip, m_port 입력받는 창 구현
+            else
+            {
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_START_Click(object sender, EventArgs e)
+        {
+            //if string 비교 금지 equal
+            var strBTNName = BTN_START.Text;
+            if (strBTNName == "시작")
+            {
+                BTN_START.Text = "멈춤";
+                Log("서버 시작됨");
+
+
+                m_listenThread = new Thread(new ThreadStart(Listen));
+                m_listenThread.IsBackground = true;
+
+                m_listenThread.Start();
+            }
+            else
+            {
+                BTN_START.Text = "시작";
+                Log("서버 멈춤");
+            }
+        }
+
+        private void Listen()
+        {
+            IPAddress ipaddress = IPAddress.Parse(m_ip);
+
+            IPEndPoint endPoint = new IPEndPoint(ipaddress, m_port);
+
+            Socket listenSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+
+            listenSocket.Bind(endPoint);
+
+            listenSocket.Listen(10);
+
+            Log("클라이언트 요청 대기중...");
+
+            // 연결 요청에 대한 수락
+            m_clientSocket = listenSocket.Accept();
+
+            Log("클라이언트 접속됨 - " + m_clientSocket.LocalEndPoint.ToString());
+
+            // Receive 스레드 호출
+            m_receiveThread = new Thread(new ThreadStart(Receive));
+            m_receiveThread.IsBackground = true;
+            m_receiveThread.Start();      // Receive() 호출
+        }
+        private void Receive()
+        {
+            while (true)
+            {
+                // 연결된 클라이언트가 보낸 데이터 수신
+                byte[] receiveBuffer = new byte[512];
+                int length = m_clientSocket.Receive(receiveBuffer,
+                    receiveBuffer.Length, SocketFlags.None);
+
+                // 엔터 처리
+                //richTextBox1.AppendText(msg);
+
+                // 디코딩
+                string msg = Encoding.UTF8.GetString(receiveBuffer);
+
+                //write log, listen
+                Showmsg("상대]" + msg);
+                Log("메시지 수신함");
+            }
+        }
+
+        private void Showmsg(string msg)
+        {
+            if (RTB_HISTORY.InvokeRequired == true)
+            {
+                RTB_HISTORY.Invoke((MethodInvoker)delegate
+                {
+                    RTB_HISTORY.AppendText(msg);
+                    RTB_HISTORY.AppendText("\r\n");
+
+                    this.Activate();
+                    RTB_HISTORY.Focus();
+
+                    RTB_HISTORY.SelectionStart = RTB_HISTORY.Text.Length;
+                    RTB_HISTORY.ScrollToCaret();
+                });
+            }
+            else
+            {
+                RTB_HISTORY.AppendText(msg);
+                RTB_HISTORY.AppendText("\r\n");
+                this.Activate();
+                RTB_HISTORY.Focus();
+
+                RTB_HISTORY.SelectionStart = RTB_HISTORY.Text.Length;
+                RTB_HISTORY.ScrollToCaret();
+            }
+        }
+
+        private void TXT_CHAT_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (TXT_CHAT.Text.Trim() != "" && e.KeyCode == Keys.Enter)
+            {
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(TXT_CHAT.Text.Trim());
+                m_clientSocket.Send(sendBuffer);
+                Log("메시지 전송됨");
+                Showmsg("나]" + TXT_CHAT.Text);
+                TXT_CHAT.Text = "";
+
+            }
+        }
+        private void Log(string msg)
+        {
+
+            if (this.LST_LOG.InvokeRequired == true)
+            {
+                this.LST_LOG.Invoke((MethodInvoker)delegate
+                {
+                    this.LST_LOG.Items.Add(string.Format("[{0}]{1}", DateTime.Now.ToString(), msg));
+                });
+            }
+            else
+            {
+                this.LST_LOG.Items.Add(string.Format("[{0}]{1}", DateTime.Now.ToString(), msg));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setIPort();
+        }
+
+    }
+}
